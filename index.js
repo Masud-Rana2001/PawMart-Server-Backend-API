0e64b7f7c40c6d2e73a85df84b1673d4a1b692bf
Masud Rana <mdmasudrana307801@gmail.com>
2025-11-13 09:47:38 +0600

    add fontent route in cors

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ const port = process.env.POST || 3000;
 
 app.use(express.json())
 app.use(cookieParser());
-app.use(cors())
+// app.use(cors())
 
 
-// app.use(cors({
-//   origin: "http://localhost:5173",
-//   credentials: true
-// }));
+app.use(cors({
+  origin: "https://paw-martmr.netlify.app"
+  
+}));
 
 
 
